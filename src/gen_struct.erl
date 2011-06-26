0cf70f5fac9e8113223d568e2cb5f90272d776de
shomodj <shomodj@gmail.com>
2011-06-26 19:18:00 +0200

    OSWC-11 pedantic bastard

--- src/gen_struct.erl
+++ src/gen_struct.erl
@@ -1,6 +1,6 @@
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 -module(gen_struct).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 -export([
     new/1,
     new/2,
@@ -11,23 +11,23 @@
     fget/3,
     fget/4
   ]).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 -define(FIELDS(M),  M:'=fields'() ).
 -define(INDEX(M,F), M:'=field'(F) ).
 -define(RECORD(M),  M:'=record'() ).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 new(Module) when is_atom(Module) ->
   ?RECORD(Module).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 new(Module,Proplist) when is_atom(Module), is_list(Proplist) ->
   Struct = ?RECORD(Module),
   Keys = proplists:get_keys(Proplist),
   lists:foldl(fun(K,S) -> S:fset(K, proplists:get_value(K,Proplist)) end, Struct, Keys).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 new_from_list(Module, List) when is_atom(Module), is_list(List) ->
   Fields = ?FIELDS(Module),
@@ -38,7 +38,7 @@ new_from_list(Module, List) when is_atom(Module), is_list(List) ->
     false -> throw({invalid_number_of_arguments,[{expected, StructLen}, {actual, ListLen}]})
   end.
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 new_from_tuple(Module, Tuple) when is_atom(Module), is_tuple(Tuple) ->
   Fields = ?FIELDS(Module),
@@ -49,7 +49,7 @@ new_from_tuple(Module, Tuple) when is_atom(Module), is_tuple(Tuple) ->
     false -> throw({invalid_number_of_arguments,[{expected, StructLen}, {actual, TupleLen}]})
   end.
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 fset(Module,Struct,Field,Value) when is_atom(Module), is_tuple(Struct), is_atom(Field) ->
   Index = ?INDEX(Module,Field),
@@ -59,7 +59,7 @@ fset(Module,Struct,Proplist) when is_atom(Module), is_tuple(Struct), is_list(Pro
   Keys = proplists:get_keys(Proplist),
   lists:foldl(fun(K,S) -> S:fset(K, proplists:get_value(K,Proplist)) end, Struct, Keys).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 fget(Module,Struct,Field) when is_atom(Module), is_tuple(Struct), is_atom(Field) ->
   Index = ?INDEX(Module,Field),
@@ -68,7 +68,7 @@ fget(Module,Struct,Field) when is_atom(Module), is_tuple(Struct), is_atom(Field)
 fget(Module,Struct,Fields) when is_atom(Module), is_tuple(Struct), is_list(Fields) ->
   [Struct:fget(F) || F <- Fields].
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 fget(Module,Struct,Field,DefVal) when is_atom(Module), is_tuple(Struct), is_atom(Field) ->
   Index = ?INDEX(Module,Field),
@@ -77,8 +77,8 @@ fget(Module,Struct,Field,DefVal) when is_atom(Module), is_tuple(Struct), is_atom
     Ret       -> Ret
   end.
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 %% TODO utils module: to_dict, to_list, to_proplist, to_json
 %% TODO dumper function
 %% TODO basic built in erlang validation
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
--- src/gen_struct_transform.erl
+++ src/gen_struct_transform.erl
@@ -1,12 +1,12 @@
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 -module(gen_struct_transform).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 -export([parse_transform/2]).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 %% TODO: clean up this file a bit
 %% TODO: create state
 %-record(state,{eof, ast, opt, record_name, module_name, record_fields}).
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 parse_transform(AST, _Options) ->
   % io:format("AST: \n~p\n",[AST]),
@@ -61,32 +61,42 @@ has_function(FunctionName, Arity, [_|Rest]) ->
 
 %%--------------------------------------------------------------------------------------------------
 
-get_last_line_number([{eof,LineNum}|_]) -> LineNum;
-get_last_line_number([_|Rest])          -> get_last_line_number(Rest).
+get_last_line_number([{eof,LineNum}|_]) -> 
+  LineNum;
+get_last_line_number([_|Rest]) -> 
+  get_last_line_number(Rest).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
-get_module_name([{attribute,_,module,ModuleName}|_]) -> ModuleName;
-get_module_name([_|Rest])                            -> get_module_name(Rest).
+get_module_name([{attribute,_,module,ModuleName}|_]) -> 
+  ModuleName;
+get_module_name([_|Rest]) -> 
+  get_module_name(Rest).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
-get_record(RecordName,[])                                             -> throw({record_does_not_exist,[{record,RecordName}]});
-get_record(RecordName,[{attribute,_,record,{RecordName,_}=Record}|_]) -> Record;
-get_record(RecordName,[_|Rest])                                       -> get_record(RecordName,Rest).
+get_record(RecordName,[]) -> 
+  throw({record_does_not_exist,[{record,RecordName}]});
+get_record(RecordName,[{attribute,_,record,{RecordName,_}=Record}|_]) -> 
+  Record;
+get_record(RecordName,[_|Rest]) -> 
+  get_record(RecordName,Rest).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 get_record_fields({_RecordName,RecordFields}) ->
   get_record_fields_loop(RecordFields,[]).
 
-get_record_fields_loop([],Acc)                                                       -> lists:reverse(Acc);
-get_record_fields_loop([{record_field,_,{atom,_,FieldName}}|Rest],Acc)               -> get_record_fields_loop(Rest,[FieldName|Acc]);
-get_record_fields_loop([{record_field,_,{atom,_,FieldName},_DefaultValue}|Rest],Acc) -> get_record_fields_loop(Rest,[FieldName|Acc]);
-get_record_fields_loop([Field|_Rest],_Acc)                                           -> throw({invalid_record_field,[{field,Field}]}).
+get_record_fields_loop([],Acc) -> 
+  lists:reverse(Acc);
+get_record_fields_loop([{record_field,_,{atom,_,FieldName}}|Rest],Acc) -> 
+  get_record_fields_loop(Rest,[FieldName|Acc]);
+get_record_fields_loop([{record_field,_,{atom,_,FieldName},_DefaultValue}|Rest],Acc) -> 
+  get_record_fields_loop(Rest,[FieldName|Acc]);
+get_record_fields_loop([Field|_Rest],_Acc) -> 
+  throw({invalid_record_field,[{field,Field}]}).
 
-
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 gen_fields_idx_fun_ast(Fields,LastLineNumber) ->
   gen_fields_idx_fun_ast_loop(Fields,LastLineNumber,2,[]). %% 2 because first element is module_name
@@ -100,20 +110,20 @@ gen_fields_idx_fun_ast_loop([Field|Fields],LastLineNumber,Index,Acc) ->
   Acc1 = Acc ++ "'=field'(" ++ atom_to_list(Field) ++") -> "++ integer_to_list(Index) ++"; \n",
   gen_fields_idx_fun_ast_loop(Fields,LastLineNumber,Index+1,Acc1).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 gen_fields_lst_fun_ast(Fields,LastLineNumber) ->
   Lst = "'=fields'() -> [ " ++ (lists:flatten([ ", " ++ atom_to_list(F) || F <- Fields]) -- "," ) ++ " ].",
   from_str_to_ast(Lst,LastLineNumber).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 insert_record_ast_fun(AST,RecordName,LastLineNumber) ->
   Str = "'=record'() -> #"++ atom_to_list(RecordName) ++"{} .",
   {InsertAST,NewLastLineNumber} = from_str_to_ast(Str,LastLineNumber),
   insert_before_eof(AST,InsertAST,NewLastLineNumber).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 insert_init_ast_fun(AST, LastLineNumber) ->
   case has_function(init, 1, AST) of
@@ -156,7 +166,7 @@ from_str_to_ast(Str, LastLineNumber) ->
   {ok, AST} = erl_parse:parse_form(Tokens),                             
   {AST, NewLastLineNumber}.
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
 
 insert_before_eof(AST, InsertAST, NewLastLineNumber) ->
   insert_before_eof_loop(AST, InsertAST, NewLastLineNumber,[]).
@@ -168,4 +178,4 @@ insert_before_eof_loop([{eof,_LastLineNumber}|_], InsertAST, NewLastLineNumber, 
 insert_before_eof_loop([ASTElemnt|Rest], InsertAST, NumberOfLines, Acc) -> 
   insert_before_eof_loop(Rest, InsertAST, NumberOfLines, [ASTElemnt|Acc]).
 
-%%----------------------------------------------------------------------------------------------------
+%%--------------------------------------------------------------------------------------------------
